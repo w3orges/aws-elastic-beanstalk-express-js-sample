b2121552e6b27bf001f35a2be75ce281c4ba8fc3
Carlos Bezerra <carlos.bezerra@syonet.com>
2022-02-03 10:17:32 -0300

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World With AWS!...'));
+app.get('/', (req, res) => res.send('Hello World With AWS! w3orges'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
